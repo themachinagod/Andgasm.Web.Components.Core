d5f7b85a554ef43577c97af6803d053d3df8d0c6
Danny Brown <machinagod@googlemail.com>
2018-10-13 21:01:33 +0100

    initial code move from vsts

---
+++ README.md
@@ -0,0 +1,28 @@
+<h2>Andgasm Web.Components.Core library:</h2>
+
+<h3>Library Information</h3>
+<p>The Andgasm Web.Components.Core library provides fully tested helper methods for static resource controll by view component libraries:
+	<ul>
+		<li>ResourceController : This controller can serve up embedded resources from referenced view component libraries </li>
+	</ul>
+</p>
+
+<p>
+    This library can be utilised on any Asp Net Core Web Site simply by adding reference from the official Nuget feed;
+</p>
+
+<ul>
+    <li><b>From Package-Manager</b>: Install-Package Andgasm.Web.Components.Core -Version 0.0.3</li>
+    <li><b>From DotNet CLI</b>: dotnet add package Andgasm.Web.Components.Core --version 0.0.3</li>
+    <li><b>From Nuget Visual Studio GUI</b>: Search for Andgasm.Web.Components.Core</li>
+</ul>
+
+<p>
+    Details of the published library can be found here:
+</p>
+
+<p>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.nuget.org/packages/Andgasm.Web.Components.Core/">https://www.nuget.org/packages/Andgasm.Web.Components.Core/</a>
+</p>
+
+<p><b>Requires DotNet Standard 2.0</b></p>
---
+++ Web.Components.Core/Andgasm.Web.Components.Core/Controllers/ResourceController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Andgasm.Web.Components.Core
+{
+    [Route("api/[controller]")]
+    public class ResourceController : Controller
+    {
+        [ResponseCache(Duration = 86400)]
+        [HttpGet("{resourcetype}/{resourcename}")]
+        public Stream GetStreamContent(string resourcetype, string resourcename)
+        {
+            Type restype = FindTypeFromAssembly($"{resourcetype}ViewComponent");
+            if (restype == null) throw new InvalidOperationException($"Specified resource type could not be resolved: {resourcetype}");
+
+            var resname = $"{restype.Module.Name.Replace(".dll", "")}.Pages.Components.{restype.Name.Replace("ViewComponent", "")}.{resourcename}";
+            if (resname == null) throw new InvalidOperationException($"Specified resource name could not be resolved: {resourcetype}");
+
+            var stream = GetManifestResourceStream(restype.Assembly, resname);
+            return stream;
+        }
+
+        private static Stream GetManifestResourceStream(Assembly assembly, string resource)
+        {
+            var correctname = assembly.GetManifestResourceNames().FirstOrDefault(x => x.ToLowerInvariant() == resource.ToLowerInvariant());
+            return assembly.GetManifestResourceStream(correctname);
+        }
+
+        private static Type FindTypeFromAssembly(string classname)
+        {
+            var asmbs = AppDomain.CurrentDomain.GetAssemblies();
+            var sourceassembly = AppDomain.CurrentDomain.GetAssemblies().Where(x => !x.IsDynamic).
+                                 FirstOrDefault(x => x.ExportedTypes.Any(y => y.Name.ToLowerInvariant().EndsWith(classname.ToLowerInvariant())));
+            if (sourceassembly == null) return null;
+            var asmblyname = sourceassembly.GetName().Name;
+            return sourceassembly.GetType($"{asmblyname}.{classname}", false, true);
+        }
+    }
+}
